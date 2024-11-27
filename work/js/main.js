f8399bacbd3442619faafb98623113d1b8be6cbf
mspakdel <ms.pakdel@gmail.com>
2024-11-27 16:57:20 +0300

    add vide elements and respective functionality to get the media stream from camera and also screen sharing

--- work/js/main.js
+++ work/js/main.js
@@ -1,2 +1,75 @@
-'use strict';
+"use strict";
 
+// import adapter from "./lib/adapter";
+const mediaStreamConstrains = {
+  // audio: true,
+  video: true,
+  backgroundBlur: true,
+};
+let localStream;
+const screenStreamConstrains = {
+  audio: true,
+  video: true,
+};
+const localVideo = document.getElementById("video");
+const localScreen = document.getElementById("screen");
+console.log(localVideo);
+console.log(localScreen);
+function gotLocalMediaStream(mediaStream) {
+  localStream = mediaStream;
+  localVideo.srcObject = mediaStream;
+}
+function handleLocalMediaStreamError(error) {
+  console.log("navigator.getUserMedia error: ", error);
+}
+function gotLocalScreenStream(mediaStream) {
+  localScreen.srcObject = mediaStream;
+}
+function handleLocalScreenStreamError(error) {
+  console.log("navigator.getUserMedia error: ", error);
+}
+navigator.mediaDevices
+  .getUserMedia(mediaStreamConstrains)
+  .then(gotLocalMediaStream)
+  .catch(handleLocalMediaStreamError);
+
+navigator.mediaDevices
+  .getDisplayMedia(screenStreamConstrains)
+  .then(gotLocalScreenStream)
+  .catch(handleLocalScreenStreamError);
+
+// posible items for constrains:
+const MediaTrackConstraintSet = {
+  width: 0, //ConstrainULong
+  height: 0, //ConstrainULong
+  sampleRate: 0, //ConstrainULong
+  sampleSize: 0, //ConstrainULong
+  channelCount: 0, //ConstrainULong
+
+  aspectRatio: 0.0, //ConstrainDouble
+  frameRate: 0.0, //ConstrainDouble
+  latency: 0.0, //ConstrainDouble
+
+  facingMode: "", //ConstrainDOMString
+  resizeMode: "", //ConstrainDOMString
+  deviceId: "", //ConstrainDOMString
+  groupId: "", //ConstrainDOMString
+
+  echoCancellation: true, //ConstrainBoolean
+  autoGainControl: true, //ConstrainBoolean
+  noiseSuppression: true, //ConstrainBoolean
+  backgroundBlur: true, //ConstrainBoolean
+};
+
+function endStream() {
+  if (localStream) {
+    localStream.getVideoTracks()[0].stop();
+  }
+}
+
+function startStream() {
+  navigator.mediaDevices
+    .getUserMedia(mediaStreamConstrains)
+    .then(gotLocalMediaStream)
+    .catch(handleLocalMediaStreamError);
+}
